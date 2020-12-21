37fa060d5f055bf6402666687154f6c138f35541
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2020-12-20 19:39:48 -0800

    added panel class with some framework

---
+++ src/Panel/Panel.java
@@ -0,0 +1,25 @@
+package Panel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public abstract class Panel extends JPanel implements ActionListener {
+    // constructor maybe used later?
+    public Panel()
+    {
+
+    }
+     @Override
+     public void actionPerformed(ActionEvent event)
+     {             //triggers the paint method which we will define in other class via overriding/polymorphism
+                repaint();
+     }
+
+
+    @Override //used in other class
+    public abstract void paint(Graphics g);
+
+
+}
