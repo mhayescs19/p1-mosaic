058db19c0273d4bb6dd23fe0973c2f65218a0a6b
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2020-12-21 20:32:24 -0800

    added sara view in a separate file because i need her method to be public

--- src/Panel/Painter.java
+++ src/Panel/Painter.java
@@ -1,25 +1,27 @@
 package Panel;
 
 import Main.SimControl;
+import View.ViewForPaint;
 
 
 import javax.swing.*;
 import java.awt.*;
 
 public class Painter extends Panel{
+    private ViewForPaint view = new ViewForPaint();
     public boolean quit = false;
     static final String name_of_panel = "Population sim";
     JFrame jFrame;
     SimControl simControl;
     Timer timer; //like objective c timer loops and does stuff every set interval
     double Time = 0; // will be used to track how long the program has been running
-    public Painter(SimControl sim, JPanel panel) // need panel
+    public Painter(SimControl sim) // need panel
     {
-        this.simControl = sim; // ass the simControl in a parameter
+        this.simControl = sim; // pass the simControl in a parameter
         jFrame = new JFrame(name_of_panel);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setVisible(true);
-        jFrame.add(this); // add this class to the jpanel
+        jFrame.add(this); // add this class to the Jframe
     }
     //starts the painting process
 
@@ -32,7 +34,7 @@ public class Painter extends Panel{
         int temp = 16; // will be replace by a dynamic var from sim control, represents delay for timer
         timer = new Timer(temp,this); // temp is delay is milliseconds, this is the class that implements the action listen
         timer.start(); // starts the timer
-        jFrame.setVisible(true); // not sure if this is necessary  but want to make sure that the jpanel is updated
+        jFrame.setVisible(true); // not sure if this is necessary  but want to make sure that the jpanel is updated with the timer and paint
     }
 
 
@@ -40,18 +42,7 @@ public class Painter extends Panel{
     public void paint(Graphics g) {
             // add paint stuff
             Time += 16; // constant will change this
-        g.setColor(new Color(0, 100, 0)); // code from sara ui, If she made panel public could call here paint code in here it should work
-        g.fillOval(300, 200, 400, 200);
-        g.fillOval(250, 170, 400, 200);
-        g.fillOval(150, 170, 400, 200);
-        g.fillOval(350, 100, 400, 200);
-        g.fillOval(325, 35, 400, 200);
-        g.fillOval(150, 50, 400, 200);
-        g.fillOval(100, 30, 400, 200);
-        g.fillOval(50, 40, 400, 200);
-        g.fillOval(30, 100, 400, 200);
-        g.fillOval(30, 120, 400, 200);
-        g.fillOval(200, 215, 400, 200);
+        view.PaintMainView(g); // sara code but with set up to be painted every pass by the timer
 
         //end condition
         if (quit)
