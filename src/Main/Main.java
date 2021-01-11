731c531f44fad4966f65641090ff45ae95e44717
mhayescs19 <mhayescs19@gmail.com>
2021-01-11 08:52:21 -0800

    added ConfigControl and connected it to SimControl

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -11,5 +11,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world P1-mosaic");
+
+        ConfigControl config = new ConfigControl();
+
+
+
     }
 }
