8a48f4b796aa02f5675adec39a04bce17e679098
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2021-01-13 19:22:22 -0800

    added the constructor for the data values for the hashmap in the Packager.java

--- src/Packager/Packager.java
+++ src/Packager/Packager.java
@@ -20,21 +20,39 @@ import java.lang.reflect.Field;
 public class Packager {
   private SimControl simControl;
   private   HashMap<String, ArrayList<AbstractMap.SimpleEntry<Integer,Object>>> hashMap; //for each type of data we have as an array list attached to it with the assign data type of a tuple pair of year and value
-    public Packager(SimControl simControl)
-    {
+    public Packager(SimControl simControl) throws IllegalAccessException {
       this.simControl = simControl;
       hashMap = new HashMap<>();// initializes the hash map
       // need to add way adding data in
+      consturctDataKeys();
     }
-    public void ConsturctDataKeys() throws IllegalAccessException {
+    public void consturctDataKeys() throws IllegalAccessException {
 
       for(Field field: simControl.getClass().getFields()) // fancy code that pulls public vars names and will be used to add names to the hash map.
       {
           System.out.println(field.getName() + " = " + field.get(simControl)); // just a tester will be used later on
-          hashMap.put(field.getName(), null); // sets the hashmap key to field name then adds a null position for now
+          if (!field.getName().equals("currentYear")) {
+            hashMap.put(field.getName(), null); // sets the hashmap key to field name then adds a null position for now
+            // this make a key for all public variables found in the class simControl
+          }
       }
       //just used in order to construct hashmap names
     }
+    public void constructorDataValue() throws NoSuchFieldException {
+
+      // thinking about pulling all the key and finding the corresponding variable name and then making the arraylist with the current year and then have the opaque object added
+      for (String variables : hashMap.keySet()) // this will only have the var we are tracking
+      {
+        Field var  = simControl.getClass().getField(variables); // grabs var by given name from hashmap then sets it into var
+        // data structure put into hash map is going to be a tuple pair and an arraylist of tuples
+        // each tuple will have a year associated with them
+        // the array list we added into the hashmap with the key of the same name of the variable
+        
+
+
+      }
+    }
+
 
 
 
